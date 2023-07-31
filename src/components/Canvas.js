afff057d0e573dd55419b5deeca5c9bcfcbcf00f
JJtrenado <f62trzaj@uco.es>
2023-07-31 16:52:04 +0200

    creating non-overlapping boxes

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,22 +2,32 @@ import React from "react";
 
 import { observer } from "mobx-react";
 import Box from "../components/Box";
+import { selectBox } from "../actions/boxActions";
 
 function Canvas({ store }) {
+
   return (
     <div className="canva">
-      {store.boxes.map((box, index) => (
-        <Box
-          id={box.id}
-          key={index}
-          color={box.color}
-          left={box.left+index*100}
-          top={box.top}
-          width={box.width}
-          height={box.height}
-          box={box}
-        />
-      ))}
+      {store.boxes.map((box, index) => {
+        const topAdjustment = index >= 6 ? Math.floor(index / 6) * 100 : 0;
+        const leftAdjustment = (index % 6) * 200;
+
+        return (
+          <div onClick={() => selectBox(box.id)}>
+            <Box
+              id={box.id}
+              key={index}
+              color={box.color}
+              left={leftAdjustment}
+              top={box.top + topAdjustment}
+              width={box.width}
+              height={box.height}
+              box={box}
+              selected={box.selected}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
