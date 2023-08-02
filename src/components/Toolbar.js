210820cccc3fdf8e472d9ed81143fe6105797281
JJtrenado <f62trzaj@uco.es>
2023-08-02 14:48:53 +0200

    using the toolbar input

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {addBox, removeLastBox, removeSelectedBox} from "../actions/boxActions";
+import {addBox, removeLastBox, removeSelectedBox, changeColor} from "../actions/boxActions";
 
 
 function Toolbar() {
@@ -8,7 +8,7 @@ function Toolbar() {
       <button onClick={addBox} >Add Box</button>
       <button onClick={removeLastBox} >Remove last Box</button>
       <button onClick={removeSelectedBox} >Remove selected Box</button>
-      <input type="color" />
+      <input type="color" onChange={changeColor}/>
       <span id={'selectMessage'} >No boxes selected</span>
     </div>
   );
