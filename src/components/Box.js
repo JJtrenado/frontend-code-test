fe9311721efc4e71704fd4f147cc1795cc7440a3
JJtrenado <f62trzaj@uco.es>
2023-08-02 14:49:38 +0200

    updating imports

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react";
-import BoxDraggable from "./BoxDraggable";
+import BoxDraggable from "./boxDraggable/BoxDraggable";
 
 function Box(props) {
   return (
